c02a28bcd212472ed8de3e085744ffe791e53321
Steve Mafoa <steve_mafoa@icloud.com>
2024-12-31 13:42:52 +0100

    feat: Implement login form with reactive validation and authentication

--- esc-harmony/src/app/features/public/login/login.component.ts
+++ esc-harmony/src/app/features/public/login/login.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  providers:[HttpClient],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
- title = "Formulaire de connexion";
+ 
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res: any) => {
+          localStorage.setItem('token', res.token);  // Stocker le token
+          this.router.navigate(['/home']);  // Rediriger vers Home
+        },
+        error: (err: any) => {
+          alert('Connexion échouée : ' + err.error.message);
+        }
+      });
+    }
+  }
 }
