37fd30b839a827523a4caca5934daf26aaf085c5
Steve Mafoa <steve_mafoa@icloud.com>
2025-01-05 23:48:12 +0100

    Refactor User model to use ES6 imports and export default

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
+import { Schema, model } from 'mongoose';
+const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
-module.exports = mongoose.model('User', userSchema);
+export default model('User', userSchema);
+
