9c59a345b1cb47bda2ad1e43f2f64d56acae14f0
Steve Mafoa <steve_mafoa@icloud.com>
2025-01-06 15:56:01 +0100

    Fix: align token name between AuthService and LoginComponent

--- esc-harmony/src/app/auth/guards/auth.guard.ts
+++ esc-harmony/src/app/auth/guards/auth.guard.ts
@@ -1,18 +1,23 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { log } from 'console';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router)
   
   const token = authService.getToken();
+  console.log("Verification du token", token);
+  
 
   if (token) {
-    // Si le token existe, accès autorisé
+    console.log('Accès autorisé');
+    
     return true;
+    
   } else {
-    // Sinon, redirige vers la page de connexion
+    console.log('Accès refusé, redirection vers /login');
     router.navigate(['/login']);
     return false;
   }
